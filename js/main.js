8e72b71b1bcb157ffb185a3b19827e2da036d7a3
Andrey Noskoff <andrenoskoff@gmail.com>
2023-03-17 16:46:49 +0300

    contacts

--- js/main.js
+++ js/main.js
@@ -2,20 +2,20 @@ const swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
     },
-    effect: "creative",
-    creativeEffect: {
-        prev: {
-            shadow: true,
-            origin: "left center",
-            translate: ["-5%", 0, -200],
-            rotate: [0, 100, 0],
-        },
-        next: {
-            origin: "right center",
-            translate: ["5%", 0, -200],
-            rotate: [0, -100, 0],
-            },
-        },
+    // effect: "creative",
+    // creativeEffect: {
+    //     prev: {
+    //         shadow: true,
+    //         origin: "left center",
+    //         translate: ["-5%", 0, -200],
+    //         rotate: [0, 100, 0],
+    //     },
+    //     next: {
+    //         origin: "right center",
+    //         translate: ["5%", 0, -200],
+    //         rotate: [0, -100, 0],
+    //         },
+    //     },
     autoplay: {
         delay: 3000,
         disableOnInteraction: false,
