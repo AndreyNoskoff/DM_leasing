531680946c5037e710b1c9eb75771916b46f6e78
Andrey Noskoff <andrenoskoff@gmail.com>
2023-03-18 16:43:17 +0300

    final_web

--- js/main.js
+++ js/main.js
@@ -2,22 +2,33 @@ const swiper = new Swiper(".swiper", {
     pagination: {
         el: ".swiper-pagination",
     },
-    // effect: "creative",
-    // creativeEffect: {
-    //     prev: {
-    //         shadow: true,
-    //         origin: "left center",
-    //         translate: ["-5%", 0, -200],
-    //         rotate: [0, 100, 0],
-    //     },
-    //     next: {
-    //         origin: "right center",
-    //         translate: ["5%", 0, -200],
-    //         rotate: [0, -100, 0],
-    //         },
-    //     },
     autoplay: {
         delay: 3000,
         disableOnInteraction: false,
     },
 });
+
+
+
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+    element.addEventListener('click', open);
+})
+function open(evt) {
+    const tabTarget = evt.currentTarget;
+    const button = tabTarget.dataset.button;
+    
+    tabItem.forEach(function(item){
+        item.classList.remove('tabs__btn-item--active');
+    });
+    tabTarget.classList.add('tabs__btn-item--active');
+    tabContent.forEach(function(item){
+        item.classList.remove('tabs__content-item--active');
+    });
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+} 
+
+
+
